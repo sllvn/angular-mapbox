edcdb7e5c0ed8fbbf2213823800374fe5c70a153
Andrew Sullivan <licyeus@gmail.com>
2014-04-25 17:23:10 -0700

    Add mapbox-marker directive

--- js/ng-mapbox.js
+++ js/ng-mapbox.js
@@ -1,25 +1,47 @@
 angular.module('ng-mapbox-example', [])
+  .controller('MapboxController', function($scope, $q) {
+    $scope.markers = [];
+
+    this.addMarker = function(lat, lng) {
+      // TODO: convert this to promises
+      // timeout is hack around map not being available until mapboxMap link function
+      setTimeout(function() {
+        var marker = L.marker([lat, lng]).addTo($scope.map);
+        $scope.markers.push(marker);
+      }, 0);
+    };
+  })
   .directive('mapboxMap', function() {
     return {
       restrict: 'E',
-      replace: true,
+      transclude: true,
       scope: {
-        centerLat: '=centerLat',
-        centerLng: '=centerLng',
-        zoomLevel: '=zoom',
-        mapboxKey: '@mapboxKey'
+        mapboxKey: '@',
+        centerLat: '@',
+        centerLng: '@',
+        zoomLevel: '@',
       },
-      link: function(scope, element, attrs) {
-        scope.map = L.mapbox.map('ng-mapbox-map', attrs.mapboxKey);
+      controller: 'MapboxController',
+      link: function($scope, $element, $attrs) {
+        $scope.map = L.mapbox.map('ng-mapbox-map', $scope.mapboxKey);
 
-        var zoomLevel = attrs.zoomLevel || 12;
-        if(attrs.centerLat && attrs.centerLng) {
-          scope.map.setView([attrs.centerLat, attrs.centerLng], zoomLevel);
+        var zoomLevel = $scope.zoomLevel || 12;
+        if($scope.centerLat && $scope.centerLng) {
+          $scope.map.setView([$scope.centerLat, $scope.centerLng], zoomLevel);
         }
       },
-      controller: function($scope) {
-        // TODO: create event listeners for adding markers, showing popups, loading feature layers
-      },
-      template: '<div id="ng-mapbox-map"></div>'
+      template: '<div id="ng-mapbox-map" ng-transclude></div>'
     }
-  });
+  })
+  .directive('mapboxMarker', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      require: '^mapboxMap',
+      transclude: true,
+      link: function($scope, $element, $attrs, mapController) {
+        mapController.addMarker($attrs.lat, $attrs.lng);
+      }
+    }
+  })
+  ;

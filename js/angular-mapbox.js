ccb244cfa9062fb0008711fe4a19d3ca552194b9
Andrew Sullivan <licyeus@gmail.com>
2014-04-28 10:31:53 -0700

    Add current location directive

--- js/angular-mapbox.js
+++ js/angular-mapbox.js
@@ -4,29 +4,41 @@ angularMapbox.controller('MapboxController', function($scope, $q) {
   $scope.markers = [];
   $scope.featureLayers = [];
 
-  this.addMarker = function(lat, lng, opts, popupContent) {
+  $scope.addMarker = function(lat, lng, popupContent, opts) {
     // timeout is hack around map not being available until mapboxMap link function
     setTimeout(function() {
       var marker = L.marker([lat, lng], opts).addTo($scope.map);
-      if(popupContent.length > 0)marker.bindPopup(popupContent);
+      if(popupContent && popupContent.length > 0) marker.bindPopup(popupContent);
       $scope.markers.push(marker);
     }, 0);
   };
 
-  this.addFeatureLayer = function(geojsonObject) {
+  $scope.addCurrentLocation = function(popupContent, opts) {
+    setTimeout(function() {
+      $scope.map.locate();
+
+      $scope.map.on('locationfound', function(e) {
+        $scope.addMarker(e.latlng.lat, e.latlng.lng);
+      });
+    }, 0);
+  }
+
+  $scope.addFeatureLayer = function(geojsonObject) {
     setTimeout(function() {
       var featureLayer = L.mapbox.featureLayer(geojsonObject).addTo($scope.map);
       $scope.featureLayers.push(featureLayer);
     }, 0);
   };
 
-  this.addFeatureLayerFromUrl = function(url) {
+  $scope.addFeatureLayerFromUrl = function(url) {
     setTimeout(function() {
       var featureLayer = L.mapbox.featureLayer().addTo($scope.map);
       featureLayer.loadURL(url);
       $scope.featureLayers.push(featureLayer);
     }, 0);
   };
+
+  this.$scope = $scope;
 });
 
 angularMapbox.directive('mapboxMap', function($compile) {
@@ -64,9 +76,6 @@ angularMapbox.directive('mapboxMarker', function($compile) {
     restrict: 'E',
     require: '^mapboxMap',
     transclude: true,
-    scope: {
-
-    },
     link: function(scope, element, attrs, controller, transclude) {
       // there's got to be a better way to programmatically access transcluded content
       var popupHTML = '';
@@ -75,9 +84,8 @@ angularMapbox.directive('mapboxMarker', function($compile) {
         if(transcluded[i].outerHTML != undefined) popupHTML += transcluded[i].outerHTML;
       }
       var opts = { draggable: typeof attrs.draggable != 'undefined' };
-      controller.addMarker(attrs.lat, attrs.lng, opts, popupHTML);
-    },
-    controller: function($scope, $compile) {
+
+      controller.$scope.addMarker(attrs.lat, attrs.lng, popupHTML, opts);
     }
   };
 });
@@ -88,10 +96,20 @@ angularMapbox.directive('featureLayer', function() {
     require: '^mapboxMap',
     link: function(scope, element, attrs, controller) {
       if(attrs.data) {
-        controller.addFeatureLayer(scope.$eval(attrs.data));
+        controller.$scope.addFeatureLayer(scope.$eval(attrs.data));
       } else if(attrs.url) {
-        controller.addFeatureLayerFromUrl(attrs.url);
+        controller.$scope.addFeatureLayerFromUrl(attrs.url);
       }
+    }
+  };
+});
+
+angularMapbox.directive('currentLocation', function() {
+  return {
+    restrict: 'E',
+    require: '^mapboxMap',
+    link: function(scope, element, attrs, controller) {
+      controller.$scope.addCurrentLocation();
     }
   };
 });

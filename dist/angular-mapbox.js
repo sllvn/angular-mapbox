2cfdf363e8a1183bcadcab38dceb15abdfc3198d
Andrew Sullivan <licyeus@gmail.com>
2014-08-09 16:01:04 -0700

    Remove standalone version of library

--- dist/angular-mapbox.js
+++ dist/angular-mapbox.js
@@ -1,14 +1,6 @@
 angular.module('angularMapbox', []);
 
 
-angular.element(document).ready(function() {
-  try {
-    angular.bootstrap(document.body, ['angularMapbox']);
-  } catch(e) {
-    console.log('angularMapbox was already loaded');
-  }
-});
-
 angular.module('angularMapbox').directive('featureLayer', function() {
   return {
     restrict: 'E',

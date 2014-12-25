a186f085794d7e84a301c865a1f94278d066a7cb
Andrew Sullivan <licyeus@gmail.com>
2014-12-24 18:27:44 -0700

    Add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,46 @@
-var gulp       = require('gulp'),
-    concat     = require('gulp-concat'),
-    uglify     = require('gulp-uglify'),
-    ngmin      = require('gulp-ngmin'),
-    jshint     = require('gulp-jshint'),
-    concatCss  = require('gulp-concat-css'),
-    livereload = require('gulp-livereload');
+(function() {
+  'use strict';
 
-gulp.task('scripts', function() {
-  return gulp.src(['src/angular-mapbox.module.js', 'src/**/*.js'])
-    .pipe(concat('angular-mapbox.js'))
-    .pipe(gulp.dest('dist'));
-});
+  var gulp       = require('gulp'),
+      concat     = require('gulp-concat'),
+      uglify     = require('gulp-uglify'),
+      ngmin      = require('gulp-ngmin'),
+      jshint     = require('gulp-jshint'),
+      livereload = require('gulp-livereload'),
+      jshint     = require('gulp-jshint'),
+      stylish    = require('jshint-stylish');
 
-gulp.task('build', function() {
-  return gulp.src(['src/angular-mapbox.module.js', 'src/**/*.js'])
-    .pipe(concat('angular-mapbox.min.js'))
-    .pipe(ngmin())
-    .pipe(uglify({ mangle: false }))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('lint', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-gulp.task('watch', ['scripts'], function() {
-  var server = livereload();
-  gulp.watch('src/**/*.js', ['scripts']);
-  gulp.watch('dist/**').on('change', function(file) {
-    server.changed(file.path);
+  gulp.task('scripts', function() {
+    return gulp.src(['src/angular-mapbox.module.js', 'src/**/*.js'])
+      .pipe(concat('angular-mapbox.js'))
+      .pipe(gulp.dest('dist'));
   });
-});
 
+  gulp.task('build', function() {
+    return gulp.src(['src/angular-mapbox.module.js', 'src/**/*.js'])
+      .pipe(concat('angular-mapbox.min.js'))
+      .pipe(ngmin())
+      .pipe(uglify({ mangle: false }))
+      .pipe(gulp.dest('dist'));
+  });
+
+  gulp.task('lint', function() {
+    return gulp.src('src/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  });
+
+  gulp.task('watch', ['scripts'], function() {
+    var server = livereload();
+    gulp.watch('src/**/*.js', ['scripts']);
+    gulp.watch('dist/**').on('change', function(file) {
+      server.changed(file.path);
+    });
+  });
+
+  gulp.task('jshint', function () {
+    return gulp.src('src/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
+  });
+})();

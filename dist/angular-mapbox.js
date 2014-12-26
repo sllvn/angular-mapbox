76f504b2d51d40b78729368db3d919941e052488
Andrew Sullivan <licyeus@gmail.com>
2014-12-25 22:58:46 -0700

    Minor refactor

--- dist/angular-mapbox.js
+++ dist/angular-mapbox.js
@@ -195,7 +195,7 @@
 (function() {
   'use strict';
 
-  angular.module('angular-mapbox').directive('marker', function($compile, mapboxService) {
+  angular.module('angular-mapbox').directive('marker', function($compile, $timeout, mapboxService) {
     var _colors = {
       navy: '#001f3f',
       blue: '#0074d9',
@@ -221,117 +221,113 @@
       require: '^mapbox',
       transclude: true,
       scope: true,
-      link: function(scope, element, attrs, controller, transclude) {
-        var opts = { draggable: attrs.draggable !== undefined };
-        var style = setStyleOptions(attrs);
-        var marker;
-
-        function setStyleOptions(attrs, defaultOpts) {
-          var opts = defaultOpts || {};
-          if(attrs.size) {
-            opts['marker-size'] = attrs.size;
-          }
-          if(attrs.color) {
-            if(attrs.color[0] === '#') {
-              opts['marker-color'] = attrs.color;
-            } else {
-              opts['marker-color'] = _colors[attrs.color] || attrs.color;
-            }
-          }
-          if(attrs.icon) {
-            opts['marker-symbol'] = attrs.icon;
-          }
-          return opts;
-        }
-
-        var addMarker = function(map, latlng, popupContent, opts, style) {
-          opts = opts || {};
-
-          var marker = L.mapbox.marker.style({ properties: style }, latlng);
-          if(popupContent && popupContent.length > 0) {
-            marker.bindPopup(popupContent);
-          }
-
-          if(mapboxService.getOptionsForMap(map).clusterMarkers && opts.excludeFromClustering !== true) {
-            controller.$scope.clusterGroup.addLayer(marker);
-          } else {
-            marker.addTo(map);
-          }
-
-          // this needs to come after being added to map because the L.mapbox.marker.style() factory
-          // does not let us pass other opts (eg, draggable) in
-          if(opts.draggable) {
-            marker.dragging.enable();
-          }
-
-          mapboxService.addMarker(marker);
-
-          return marker;
-        };
-
-        var addCurrentLocation = function(map, popupContent, opts, style) {
-          style = setStyleOptions(style, { 'marker-color': '#000', 'marker-symbol': 'star' });
-          opts.excludeFromClustering = true;
-
-          map.on('locationfound', function(e) {
-            marker = addMarker(map, [e.latlng.lat, e.latlng.lng], null, opts, style);
-          });
-
-          map.locate();
-        };
-
-        controller.getMap().then(function(map) {
-          map.on('popupopen', function() {
-            // ensure that popups are compiled
-            var popup = angular.element(document.getElementsByClassName('leaflet-popup-content'));
-            $compile(popup)(scope);
-            if(!scope.$$phase) {
-              scope.$digest();
-            }
-          });
-
-          setTimeout(function() {
-            // there's got to be a better way to programmatically access transcluded content
-            var popupHTML = '';
-            var transcluded = transclude(scope, function() {});
-            for(var i = 0; i < transcluded.length; i++) {
-              if(transcluded[i].outerHTML !== undefined) {
-                popupHTML += transcluded[i].outerHTML;
-              }
-            }
-
-            if(attrs.currentLocation !== undefined) {
-              addCurrentLocation(map, null, opts, style);
-            } else {
-              if(popupHTML) {
-                var popup = angular.element(popupHTML);
-                $compile(popup)(scope);
-                if(!scope.$$phase) {
-                  scope.$digest();
-                }
-
-                var newPopupHTML = '';
-                for(i = 0; i < popup.length; i++) {
-                  newPopupHTML += popup[i].outerHTML;
-                }
-
-                marker = addMarker(map, [attrs.lat, attrs.lng], newPopupHTML, opts, style);
-              } else {
-                marker = addMarker(map, [attrs.lat, attrs.lng], null, opts, style);
-              }
-
-              element.bind('$destroy', function() {
-                if(mapboxService.getOptionsForMap(map).clusterMarkers) {
-                  controller.$scope.clusterGroup.removeLayer(marker);
-                } else {
-                  map.removeLayer(marker);
-                }
-              });
-            }
-          }, 0);
-        });
-      }
+      link: link
     };
+
+    function link(scope, element, attrs, controller, transclude) {
+      var opts = { draggable: attrs.draggable !== undefined };
+      var style = setStyleOptions(attrs);
+      var marker;
+
+      controller.getMap().then(function(map) {
+        map.on('popupopen', function() {
+          // ensure that popups are compiled
+          var popup = angular.element(document.getElementsByClassName('leaflet-popup-content'));
+          $compile(popup)(scope);
+          if(!scope.$$phase) {
+            scope.$digest();
+          }
+        });
+
+        $timeout(function() {
+          // there's got to be a better way to programmatically access transcluded content
+          var popupHTML = '';
+          var transcluded = transclude(scope, function() {});
+          for(var i = 0; i < transcluded.length; i++) {
+            if(transcluded[i].outerHTML !== undefined) {
+              popupHTML += transcluded[i].outerHTML;
+            }
+          }
+
+          if(attrs.currentLocation !== undefined) {
+            style = setStyleOptions(style, { 'marker-color': '#000', 'marker-symbol': 'star' });
+            opts.excludeFromClustering = true;
+            map.on('locationfound', function(e) {
+              marker = addMarker(controller.$scope, map, [e.latlng.lat, e.latlng.lng], null, opts, style);
+            });
+            map.locate();
+          } else {
+            if(popupHTML) {
+              var popup = angular.element(popupHTML);
+              $compile(popup)(scope);
+              if(!scope.$$phase) {
+                scope.$digest();
+              }
+
+              var newPopupHTML = '';
+              for(i = 0; i < popup.length; i++) {
+                newPopupHTML += popup[i].outerHTML;
+              }
+
+              marker = addMarker(controller.$scope, map, [attrs.lat, attrs.lng], newPopupHTML, opts, style);
+            } else {
+              marker = addMarker(controller.$scope, map, [attrs.lat, attrs.lng], null, opts, style);
+            }
+
+            element.bind('$destroy', function() {
+              if(mapboxService.getOptionsForMap(map).clusterMarkers) {
+                controller.$scope.clusterGroup.removeLayer(marker);
+              } else {
+                map.removeLayer(marker);
+              }
+            });
+          }
+        });
+      });
+    }
+
+    function setStyleOptions(attrs, defaultOpts) {
+      var opts = defaultOpts || {};
+      if(attrs.size) {
+        opts['marker-size'] = attrs.size;
+      }
+      if(attrs.color) {
+        if(attrs.color[0] === '#') {
+          opts['marker-color'] = attrs.color;
+        } else {
+          opts['marker-color'] = _colors[attrs.color] || attrs.color;
+        }
+      }
+      if(attrs.icon) {
+        opts['marker-symbol'] = attrs.icon;
+      }
+      return opts;
+    }
+
+    function addMarker(scope, map, latlng, popupContent, opts, style) {
+      opts = opts || {};
+
+      var marker = L.mapbox.marker.style({ properties: style }, latlng);
+      if(popupContent && popupContent.length > 0) {
+        marker.bindPopup(popupContent);
+      }
+
+      if(mapboxService.getOptionsForMap(map).clusterMarkers && opts.excludeFromClustering !== true) {
+        scope.clusterGroup.addLayer(marker);
+      } else {
+        marker.addTo(map);
+      }
+
+      // this needs to come after being added to map because the L.mapbox.marker.style() factory
+      // does not let us pass other opts (eg, draggable) in
+      if(opts.draggable) {
+        marker.dragging.enable();
+      }
+
+      mapboxService.addMarker(marker);
+
+      return marker;
+    }
   });
 })();
 
